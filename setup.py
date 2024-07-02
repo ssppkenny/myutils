11dfb157522cde6ca96815523dc7ae84bf289152
Sergey Mikhno <sergey.mikhno@gmail.com>
2024-07-02 17:25:03 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+import numpy 
+
+from os import getenv
+
+PLATFORM = sys.platform
+
+if getenv('LIBLINK'):
+    PLATFORM = 'android'
+
+
+setup(name="utils",
+      version="0.0.1",
+      ext_modules = [
+        Extension('utils', ['utils.c', 'pdf.m'], libraries=[], library_dirs=[], include_dirs=[numpy.get_include()])
+    ]
+)
