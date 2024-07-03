2d2f66a1e7c208b2c7d031a37bdb864be15def0d
Sergey Mikhno <sergey.mikhno@gmail.com>
2024-07-03 12:50:54 +0300

    0.0.8

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ PLATFORM = sys.platform
 if getenv('LIBLINK'):
     PLATFORM = 'android'
 
-extension = Extension('utils', ['utils.c', 'pdf.m'], libraries=[], library_dirs=[], include_dirs=[numpy.get_include()], extra_compile_args=["-framework CoreFoundation -framework CoreGraphics"])
+extension = Extension('utils', ['utils.c', 'pdf.m'], libraries=[], library_dirs=[], include_dirs=[numpy.get_include()])
 setup(name="utils",
-      version="0.0.6",
+      version="0.0.8",
       ext_modules = [
         extension
     ]
