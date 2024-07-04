57e9ac893c12ddfc01e52c81fea02c11ced8833b
Sergey Mikhno <sergey.mikhno@gmail.com>
2024-07-04 07:39:04 +0300

    0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ library_dirs = ['lib/' + os.environ['ARCH']]
 
 extension = Extension('utils', ['utils.c', 'pdf.c'], libraries=["pdfium"], library_dirs=library_dirs, include_dirs=["include", numpy.get_include()])
 setup(name="utils",
-      version="0.1.1",
+      version="0.1.2",
       ext_modules = [
         extension
     ]
